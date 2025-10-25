d71028cb08e7c08e4556d382204866821c0d1b1c
Kiran Kumar <kiran25cd@gmail.com>
2025-10-25 04:50:44 +0000

    Updated data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 
 
-insert into tbl_doc (id, description, link, title, user_id) values(01, 'Yellow rose png', 'https://www.gstatic.com/webp/gallery3/1.png', 'yellow Rose', 01);
+insert into tbl_doc (id, description, link, title, user_id) values(01, 'RED rose png', 'https://www.gstatic.com/webp/gallery3/1.png', 'RED Rose', 01);
 insert into tbl_doc (id, description, link, title, user_id) values(02, 'baby tux for user page', 'https://www.gstatic.com/webp/gallery3/2.png', 'Baby Tux', 01);
 insert into tbl_doc (id, description, link, title, user_id) values(03, 'Ludo PNG transparency', 'https://www.gstatic.com/webp/gallery3/3.png', 'Ludo', 01);
 insert into tbl_doc (id, description, link, title, user_id) values(04, 'hill mountain river', 'http://www.gstatic.com/webp/gallery/1.jpg', 'scenery', 02);
